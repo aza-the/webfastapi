d6dd15fbbfe50bffd1f00712645c11e58c79569c
Azamat Dzabaev <azamatdzabaev310@gmail.com>
2022-10-30 19:31:54 +0300

    404 handler added

--- app/main.py
+++ app/main.py
@@ -14,6 +14,12 @@ app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, __):
+    return templates.TemplateResponse("error404.html", context={"request": request})
+
 @app.get('/', response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("root.html", context={"request": request})
+
+
---
+++ app/static/error404.js
@@ -0,0 +1,6 @@
+console.log("ERROR404.JS started");
+
+const main_page = document.getElementById("btn1")
+main_page.addEventListener("click", () => {
+    location.href = "/";
+}); 
