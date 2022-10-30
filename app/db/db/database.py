18c41151eba5ebe4f63efe062b056ab43832e21f
Azamat Dzabaev <azamatdzabaev310@gmail.com>
2022-10-30 23:50:41 +0300

    changed db congig

--- app/db/db/database.py
+++ app/db/db/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# should be equal to db in alembic.ini
 SQLALCHEMY_DATABASE_URL = 'postgresql://flatsuser:password@flats-postgres/flatsdb'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
