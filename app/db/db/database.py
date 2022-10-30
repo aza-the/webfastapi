ea6f36579404ab6d38e63a1e3761a2b5462bccc4
Azamat Dzabaev <azamatdzabaev310@gmail.com>
2022-10-30 23:49:54 +0300

    changed db congig

--- app/db/db/database.py
+++ app/db/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://flatsuser:password@localhost/flatsdb'
+SQLALCHEMY_DATABASE_URL = 'postgresql://flatsuser:password@flats-postgres/flatsdb'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
